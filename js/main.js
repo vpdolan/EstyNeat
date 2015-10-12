d976d7e2dbb7de812dc2a0afaf2971ee6d4120fb
Vinnie Dolan <vpdolan@gmail.com>
2015-10-11 20:04:54 -0400

    udpated sidebars and header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//IIFE
+
 (function () {
 
 
@@ -5,5 +7,16 @@ var test = etsy;
 
 
   console.dir(test);
+
+  <div class="Container" <</div>
+   <script type="text/template" id=itemTemplate">
+   <div>
+    <p> <%= title %></p>
+    <p> class="price"> <%= price %> </p> 
+      <img src="<%= Images [0].url_170x135%>"> 
+
+    </div>
+      <hr>
+       </script>
 
 }());
